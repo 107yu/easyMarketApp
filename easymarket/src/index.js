aaeebe59f9c21102e8ac0787fec40c4726e4998c
yjj <yujuanjuan107@163.com>
2019-07-25 09:02:13 +0800

    引入antd-mobile

--- easymarket/src/index.js
+++ easymarket/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//关联mobx
 import {Provider} from "mobx-react"
+//仓库
 import  store from "./store/index"
+//页面组件
 import IndexPage from "./views/indexPage"
-
+//样式
+import "./index.css"
+import 'antd-mobile/dist/antd-mobile.css';
 ReactDOM.render(<Provider {...store}>
     <IndexPage />
 </Provider>, document.getElementById('root'));
--- easymarket/src/views/indexPage.js
+++ easymarket/src/views/indexPage.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
 export default class IndexPage extends Component {
     render() {
         return (
             <div>
                 IndexPage
+              
             </div>
         )
     }
