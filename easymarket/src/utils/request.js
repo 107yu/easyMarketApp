2f57fa17ba909c2a8e7420c4a34412438b22e256
yjj <yujuanjuan107@163.com>
2019-07-26 21:51:09 +0800

    ok

--- easymarket/src/utils/request.js
+++ easymarket/src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8888',
+  baseURL: 'http://169.254.32.130:8888',
   // withCredentials: true, 
   timeout: 5000 // request timeout
 })
