6408c8c1c942d34baf06037b8ecf06c842f3add3
yjj <yujuanjuan107@163.com>
2019-07-30 19:13:10 +0800

    合并分类

--- easymarket/src/store/modules/collect.js
+++ easymarket/src/store/modules/collect.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import {getcollect,addCollect} from "../../services/index"
-=======
-import {getcollect,removeCollect} from "../../services/index"
->>>>>>> yu
 import {observable,action} from "mobx"
 
 export default class collect{
@@ -14,19 +10,10 @@ export default class collect{
            this.getcollectList=res
         })
     }
-<<<<<<< HEAD
     //添加收藏
     @action addcollect =async(params)=>{
         const data=await addCollect(params)
         this.collectInfo=data.data.type==="delete"?0:1
         console.log(data.data.type,"collect")
-=======
-     //取消收藏
-     @action cancelCollect= async (info)=>{
-       let data=await  removeCollect(info)
-       if(data.errno===0){
-        this.getcollectData({typeId:0})
-       }
->>>>>>> yu
     }
 }
