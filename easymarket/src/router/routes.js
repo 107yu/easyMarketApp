deec24f8b83674d4a85bd3e0ae2edbf50c51e1f6
yjj <yujuanjuan107@163.com>
2019-07-30 10:08:44 +0800

    完善购物车

--- easymarket/src/router/routes.js
+++ easymarket/src/router/routes.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Loadable from "react-loadable"
+import { Toast} from 'antd-mobile';
 import Catego from "../views/classify/categoItem/categoItem"
 import ProductDetail from "../views/classify/productDetail/productDetail"
 //import Login from '../views/login'
 function Loading(){
     return <div className="loading">
-        <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564210029472&di=3da7da476dc8bd9ac1d42afaa7c05d9a&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F611bb3198c03cfefea188d170f33f27f1611c8e8a3ea-o8nm2q_fw658" alt=""/>
+      { Toast.loading('loading')}
     </div>
 }
 const Login=Loadable({
--- easymarket/src/utils/request.js
+++ easymarket/src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8888',
+  baseURL: 'http://169.254.12.157:8888',
   // withCredentials: true, 
   timeout: 5000 // request timeout
 })
