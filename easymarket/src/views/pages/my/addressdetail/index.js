d19880227891666c14b24f0e9527f01aee26ea8d
yjj <yujuanjuan107@163.com>
2019-07-31 17:41:57 +0800

    添加地址

--- easymarket/src/views/pages/my/addressdetail/index.js
+++ easymarket/src/views/pages/my/addressdetail/index.js
@@ -1,62 +1,59 @@
 import React from 'react'
 import './index.scss'
-import { Link } from 'react-router-dom'
 import Addrest from '../../../../components/addrest/addrest'
-
+import Header from "../../../../components/goback/goBack"
 import { inject, observer } from "mobx-react"
-
 @inject('addres')
 @observer
-
-
 class Addressdetail extends React.Component {
     constructor(){
         super()
         this.state={
-            isaddress:false
+            isaddress:false,
+            item:null,
         }
     }
     componentDidMount() {
         this.props.addres.getaddresData()
     }
-    addresDo() {
+    addresDo(item) {//改变状态--添加地址--更改地址--
         let { isaddress } = this.state;
         this.setState({
-            isaddress:!isaddress
+            isaddress:!isaddress,
+            item,
         })
+    }
+    delete(id){
+        this.props.addres.deleteAddress({id})
     }
     render() {
         let { getaddresList } = this.props.addres;
-        let { isaddress } = this.state;
+        let { isaddress,item } = this.state;
         if(isaddress){
-            return <Addrest />
+            return <Addrest {...this.props} changeAddress={()=>{this.addresDo()}} item={item}/>
         }
         return <div id='addressPage'>
-            <header className='header'>
-                <Link to='/pages/my'>◁</Link>
-                <div>地址管理</div>
-                <div></div>
-            </header>
+            <Header {...this.props} title="地址管理"></Header>
+            <div className='addressList'>
             {
                 getaddresList.length>0 && getaddresList.map((item, index) => {
-                    return <div className='addressList'>
-                        <div className='addressItem'>
-                            <div className='isChooseAddress'></div>
-                            <div className='addressMsg'>
-                                <div className='concatName'>{item.name}</div>
-                                <div className='addressDetail'>
-                                    <div>{item.mobile}</div>
-                                    <div>{item.address}</div>
-                                    <div>{item.full_region}</div>
-                                </div>
-                                <div className='deleteAddress'>
-                                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyBAMAAADsEZWCAAAALVBMVEVmZmZtbW1ubm5wcHCJiYmKioqOjo6Tk5OUlJSVlZXm5ubn5+fv7+/y8vL////PAON/AAAAa0lEQVQ4y2N4hwsw0F8mgIGBgRWrDAMIoMokMKADNoIy6O5gwCAHgUwoThkGEmWexiFTyDIP+JCpUZmBkXnqh0wRH9shA5dGBe4hSbxlRJJpqEGSOcmBJLMYJS9aIcm8noQkobluoModNAAADGfUnhuYO1kAAAAASUVORK5CYII=" alt="" />
-                                </div>
+                    return  <div className='addressItem' key={item.id}>
+                        <div className={item.is_default?'isChooseAddress':''}></div>
+                        <div className='addressMsg'>
+                            <div className='concatName'>{item.name}</div>
+                            <div className='addressDetail' onClick={()=>{this.addresDo(item)}}>
+                                <div>{item.mobile}</div>
+                                <div className="addressDetail_address">{item.full_region}</div>
+                                <div className="addressDetail_address">{item.address}</div>
+                            </div>
+                            <div className='deleteAddress' onClick={()=>{this.delete(item.id)}}>
+                                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyBAMAAADsEZWCAAAALVBMVEVmZmZtbW1ubm5wcHCJiYmKioqOjo6Tk5OUlJSVlZXm5ubn5+fv7+/y8vL////PAON/AAAAa0lEQVQ4y2N4hwsw0F8mgIGBgRWrDAMIoMokMKADNoIy6O5gwCAHgUwoThkGEmWexiFTyDIP+JCpUZmBkXnqh0wRH9shA5dGBe4hSbxlRJJpqEGSOcmBJLMYJS9aIcm8noQkobluoModNAAADGfUnhuYO1kAAAAASUVORK5CYII=" alt="" />
                             </div>
                         </div>
                     </div>
                 })
             }
+            </div>
             <div className='addAddress'
                 type="primary"
                 onClick={() => {
@@ -69,23 +66,4 @@ class Addressdetail extends React.Component {
         </div>
     }
 }
-
 export default Addressdetail
-
-
-
-
-
-// <div className='addressMsg'>
-//                     <div className='concatName'>XXX</div>
-//                     <div className='addressDetail'>
-//                         <div>15026346812</div>
-//                         <div>XXXXXXX</div>
-//                         <div>XXXXXXX</div>
-//                     </div>
-//                 </div>
-//                 <div className='addAddress'>
-//                     <a className='am-button'>
-//                         <span>新建地址</span>
-//                     </a>
-//                 </div>
